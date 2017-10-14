5c236444ce711a105001a479cd90ff41f9e83437
enidmero <enid.mero@gmail.com>
2017-10-14 17:04:40 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,34 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import mechanize
+import re # write a regex to get the parameters expected by __doPostBack
+from bs4 import BeautifulSoup
+from time import sleep
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+br = mechanize.Browser()
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+response = br.open("http://www.gfsc.gg/The-Commission/Pages/Regulated-Entities.aspx?auto_click=1")
+# satisfy the __doPostBack function to navigate to different pages 
+for pg in range(2,5):
+    br.select_form(nr=0) # the only form on the page
+    br.set_all_readonly(False) # to set the __doPostBack parameters
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+    # BeautifulSoup for parsing
+    soup = BeautifulSoup(response, 'lxml')
+    table = soup.find('table', {'class': 'RegulatedEntities'})
+    records = table.find_all('tr', {'style': ["background-color:#E4E3E3;border-style:None;", "border-style:None;"]})
+
+    for rec in records[:1]:
+        print 'Company name:', rec.a.string
+
+    # disable 'Search' and 'Clear filters'
+    for control in br.form.controls[:]:
+        if control.type in ['submit', 'image', 'checkbox']:
+            control.disabled = True
+
+    # get parameters for the __doPostBack function
+    for link in soup("a"):
+        if link.string == str(page):
+            next = re.search("""<a href="javascript:__doPostBack\('(.*?)','(.*?)'\)">""", str(link))
+            br["__EVENTTARGET"] = next.group(1)
+            br["__EVENTARGUMENT"] = next.group(2)
+    sleep(1)    
+    response = br.submit()
